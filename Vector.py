a8bfb82cffa72ecfb2547c20d286276e374ab8bf
Abhi-1U <PerricoQ@outlook.com>
2020-05-27 18:35:23 +0530

    Vector : 4 New Methods

--- Vector.py
+++ Vector.py
@@ -12,6 +12,9 @@ from PyrixExceptions import incompaitableTypeException, divisionErrorException, 
 import math
 import copy
 
+# Vector is treated like a List
+# Unlike Matrix which is Nested List
+
 
 class vectorData:
     def __init__(self, dims, data):
@@ -38,16 +41,34 @@ class Vector:
         self.vector = vectorData(dims, data)
 
     def __str__(self):
-        pass
+        stringV = str()
+        stringV = "Vector:\n"
+        for item in self.vector.data:
+            stringV += str(item)+"\n"
+        stringV += "Dimensions :" + \
+            str(self.vector.dimensions)
+        return stringV
 
     def __repr__(self):
         pass
 
-    def __add__(self, *args):
-        pass
+    def __add__(self, Vector2):
+        if(self.vector.dimensions == Vector2.vector.dimensions):
+            sum = []
+            for i in range(self.vector.dimensions):
+                sum.append(self.vector.data[i]+Vector2.vector.data[i])
+            return Vector(dims=self.vector.dimensions, data=sum)
+        else:
+            raise incompaitableTypeException
 
-    def __sub__(self, *args):
-        pass
+    def __sub__(self, Vector2):
+        if(self.vector.dimensions == Vector2.vector.dimensions):
+            diff = []
+            for i in range(self.vector.dimensions):
+                diff.append(self.vector.data[i]-Vector2.vector.data[i])
+            return Vector(dims=self.vector.dimensions, data=diff)
+        else:
+            raise incompaitableTypeException
 
     def __mul__(self, *args):
         pass
@@ -55,8 +76,11 @@ class Vector:
     def __truediv__(self, m2):
         raise divisionErrorException
 
-    def __eq__(self, m2):
-        pass
+    def __eq__(self, vector2):
+        if(self.vector.dimensions == vector2.vector.dimensions)and (self.vector.data == vector2.vector.data):
+            return True
+        else:
+            return False
 
     def __floordiv__(self, m2):
         raise divisionErrorException
@@ -82,8 +106,14 @@ class Vector:
     def __invert__(self):
         raise bitWiseOnMatrix
 
+    def __abs__(self):
+        return self.vectorNorm()
+
+    def __pow__(self):
+        return self.dotProduct(v2=self)
+
     def copy(self):
-        pass
+        return copy.deepcopy(self)
 
     def dotProduct(self, v2):
         assert self.vector.dimensions == v2.vector.dimensions, "incompaitableTypeException"
@@ -103,31 +133,45 @@ class Vector:
             self.vector.data[i] /= normValue
         return self
 
+    def scaleVector(self, scalar):
+        data = self.vector.data
+        for i in range(self.vector.dimensions):
+            data[i] *= scalar
+        return self
+
     def crossProduct(self, v2):
         raise NotImplementedError
 
     def orthogonalityVector(self):
         raise NotImplementedError
 
-    def matrixMultiplication(self, m1):
-        raise NotImplementedError
-
 
 def unitVector(dims):
     VectorData = []
-    for i in range(dims):
-        VectorData.append([])
-        VectorData[i].append(1)
+    for _i in range(dims):
+        VectorData.append(1)
     return Vector(dims=dims, data=VectorData)
 
 
 def zeroVector(dims):
     VectorData = []
-    for i in range(dims):
-        VectorData.append([])
-        VectorData[i].append(0)
+    for _i in range(dims):
+        VectorData.append(0)
     return Vector(dims=dims, data=VectorData)
 
 
 def randomVector(scale, type):
     pass
+
+
+def linearVector(dims, minVal, maxVal):
+    assert minVal <= maxVal
+    'Not suitable Range'
+    assert dims > 1
+    'Dimensions too small'
+    data = []
+    gap = (maxVal-minVal)/(dims-1)
+    for i in range(dims):
+        data.append(minVal+gap*i)
+    linVector = Vector(dims=dims, data=data)
+    return linVector
